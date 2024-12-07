3e68d71709fbf6b8406b4acf0d9bda771fb98c79
khangnguyennn <137969145+khangnguyennn@users.noreply.github.com>
2024-12-07 20:58:02 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 jiuse#!/bin/bash
 
-curl -s https://raw.githubusercontent.com/CryptoBureau01/logo/main/logo.sh | bash
+curl -s https://raw.githubusercontent.com/vnbnode/binaries/main/Logo/logo.sh | bash
 sleep 5
 
 # Function to print info messages
